1eba7f31e014538a25d7859c69dcd77ef88d791e
joaovieiraantunes <87206306+joaovieiraantunes@users.noreply.github.com>
2024-03-07 11:07:43 +0000

    Add: Addition and Subtraction operations

--- metajulia_evaluator.jl
+++ metajulia_evaluator.jl
@@ -13,8 +13,28 @@ function metajulia_repl()
 end
 
 function metajulia_eval(expr)
-    #If the expression is a single number, return it
+
+    # If the expression is a single number, return it
     if isa(expr, Number)
         return expr
     end
+
+    # If the expression is a string, return it
+    if isa(expr, String)
+        return expr
+    end
+
+    # If the expression is a call to the addition function, evaluate it
+    if isa(expr, Expr) && expr.head == :call && expr.args[1] == :+
+        return sum(metajulia_eval(arg) for arg in expr.args[2:end])
+    end
+
+    # If the expression is a call to the subtraction function, evaluate it
+    if isa(expr, Expr) && expr.head == :call && expr.args[1] == :-
+        return metajulia_eval(expr.args[2]) - sum(metajulia_eval(arg) for arg in expr.args[3:end])
+    end
+
+    # Error handling, simply return the expression with a message "Unknown expression" and its type
+    println("Unknown expression: ", expr, " of type ", typeof(expr))
+
 end
