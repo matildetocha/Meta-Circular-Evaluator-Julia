28461f8fab603da1218147b6b6ecb73533b5d13c
DiogoSoarex <diogomgsoares16@gmail.com>
2024-03-07 00:27:14 +0000

    Fix: Updated the metajulia_repl exit case

--- metajulia_evaluator.jl
+++ metajulia_evaluator.jl
@@ -2,13 +2,19 @@ function metajulia_repl()
     while true
         print(">> ") 
         line = readline() 
+        #If the user types "exit", break the loop
+        if line == "exit"
+            break
+        end
         expr = Meta.parse(line)
-        #We need to make our own eval function
-        # result = our_eval(expr)
-        # println(result)
+        result = metajulia_eval(expr)
+        println(result)
     end
 end
 
-function metajulia_eval()
-#We need to make our own eval function
+function metajulia_eval(expr)
+    #If the expression is a single number, return it
+    if isa(expr, Number)
+        return expr
+    end
 end
