691fd161678b8896f920f174aa5c592fd81a3743
nunoribeiro02 <nuno.m.p.ribeiro@tecnico.ulisboa.pt>
2024-03-08 21:24:00 +0000

    feat: abstraction for calls

--- metajulia_evaluator.jl
+++ metajulia_evaluator.jl
@@ -16,31 +16,12 @@ function metajulia_eval(expr)
 
     if is_self_evaluating(expr)
         return expr
+    elseif is_call(expr)
+        return process_call(expr)
+    else
+        # Error handling, simply return the expression with a message "Unknown expression" and its type
+        println("Unknown expression: ", expr, " of type ", typeof(expr))
     end
-
-    # If the expression is a call to the addition function, evaluate it
-    if isa(expr, Expr) && expr.head == :call && expr.args[1] == :+
-        return sum(metajulia_eval(arg) for arg in expr.args[2:end])
-    end
-
-    # If the expression is a call to the subtraction function, evaluate it
-    if isa(expr, Expr) && expr.head == :call && expr.args[1] == :-
-        return metajulia_eval(expr.args[2]) - sum(metajulia_eval(arg) for arg in expr.args[3:end])
-    end
-    
-    #If the expression is a call to the multiplication function, evaluate it
-    if isa(expr, Expr) && expr.head == :call && expr.args[1] == :*
-        return prod(metajulia_eval(arg) for arg in expr.args[2:end])
-    end
-
-    #If the expression is a call to the division function, evaluate it
-    if isa(expr, Expr) && expr.head == :call && expr.args[1] == :/
-        return metajulia_eval(expr.args[2]) / metajulia_eval(expr.args[3])
-    end
-
-    # Error handling, simply return the expression with a message "Unknown expression" and its type
-    println("Unknown expression: ", expr, " of type ", typeof(expr))
-
 end
 
 
@@ -48,3 +29,29 @@ function is_self_evaluating(expr)
     # If the expression is a number, string or boolean, then it's self evaluating and return true
     return isa(expr, Number) || isa(expr, String) || isa(expr, Boolean)
 end        
+
+function is_call(expr)
+    return expr.head == :call
+end
+
+function process_call(expr)
+    # If the expression is a call to the addition function, evaluate it
+    if expr.args[1] == :+
+        return sum(metajulia_eval(arg) for arg in expr.args[2:end])
+    end
+
+    # If the expression is a call to the subtraction function, evaluate it
+    if expr.args[1] == :-
+        return metajulia_eval(expr.args[2]) - sum(metajulia_eval(arg) for arg in expr.args[3:end])
+    end
+    
+    #If the expression is a call to the multiplication function, evaluate it
+    if expr.args[1] == :*
+        return prod(metajulia_eval(arg) for arg in expr.args[2:end])
+    end
+
+    #If the expression is a call to the division function, evaluate it
+    if isa(expr, Expr) && expr.head == :call && expr.args[1] == :/
+        return metajulia_eval(expr.args[2]) / metajulia_eval(expr.args[3])
+    end
+end
